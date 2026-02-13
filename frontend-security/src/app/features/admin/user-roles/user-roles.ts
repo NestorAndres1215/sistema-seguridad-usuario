5234f06e07f12ac04ed151041c9643ff9d9c2149
NestorAndres1215 <atiroandres@gmail.com>
2026-02-13 12:27:07 -0500

    subiendo

--- frontend-security/src/app/features/admin/user-roles/user-roles.ts
+++ frontend-security/src/app/features/admin/user-roles/user-roles.ts
@@ -11,12 +11,11 @@ import { FormsModule } from '@angular/forms';
 export class UserRoles {
   role: any[] | null = null;
   currentPage = 1;
-  itemsPerPage = 10; // valor por defecto
+  itemsPerPage = 10;
 
   constructor(private roleService: RoleService) { }
 
   ngOnInit(): void {
-    // Leer valor guardado en localStorage
     const savedItems = localStorage.getItem('itemsPerPage');
     if (savedItems) {
       this.itemsPerPage = parseInt(savedItems, 10);
@@ -28,31 +27,29 @@ export class UserRoles {
     this.roleService.getAllRole().subscribe({
       next: (data) => (this.role = data),
       error: (err) => {
-        console.error('Error cargando usuarios:', err);
+
         this.role = [];
       },
     });
   }
 
-  // Total de páginas
   get totalPages(): number {
     return this.role ? Math.ceil(this.role.length / this.itemsPerPage) : 0;
   }
 
-  // Usuarios paginados
+
   paginatedUsers(): any[] {
     if (!this.role) return [];
     const startIndex = (this.currentPage - 1) * this.itemsPerPage;
     return this.role.slice(startIndex, startIndex + this.itemsPerPage);
   }
 
-  // Cambiar cantidad por página
   onItemsPerPageChange(): void {
     localStorage.setItem('itemsPerPage', this.itemsPerPage.toString());
     this.currentPage = 1;
   }
 
-  // Navegación
+
   nextPage(): void {
     if (this.currentPage < this.totalPages) this.currentPage++;
   }
