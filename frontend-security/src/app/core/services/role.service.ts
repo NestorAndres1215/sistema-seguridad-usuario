9c2c9e380c67ba328fe9a9e5f908d90991707b55
NestorAndres1215 <atiroandres@gmail.com>
2026-02-13 12:37:19 -0500

    subiendo

--- frontend-security/src/app/core/services/role.service.ts
+++ frontend-security/src/app/core/services/role.service.ts
@@ -7,11 +7,20 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class RoleService {
-    private backendUrl = environment.backendUrl;
+  private backendUrl = environment.backendUrl;
 
   constructor(private http: HttpClient) { }
 
-    getAllRole(): Observable<any[]> {
-      return this.http.get<any[]>(`${this.backendUrl}/role/list`);
-    }
+  getAllRole(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.backendUrl}/role/list`);
+  }
+  getRoleById(id: number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.backendUrl}/roles/${id}`);
+  }
+  createRole(role: any): Observable<any> {
+    return this.http.post<any>(`${this.backendUrl}/roles`, role);
+  }
+  deleteRole(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.backendUrl}/roles/${id}`);
+  }
 }
